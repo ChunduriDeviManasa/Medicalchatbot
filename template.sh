9d2c17c75963f1a780baedd62d166829b6fcfe29
ChunduriDeviManasa <chunduridevimanasa@gmail.com>
2026-02-17 17:54:33 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+#creating directories
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+
+echo "Directory and files created successfully!"
