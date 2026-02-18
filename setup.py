7be2c6598b397886cf9a5a99fa55f49ac26cc9e5
ChunduriDeviManasa <chunduridevimanasa@gmail.com>
2026-02-18 16:44:51 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MedicalChatbot",
+    version="0.0.1",
+    author="Chunduri Devi Manasa",
+    author_email="Medical chatbot project",
+    packages=find_packages(),
+    install_requires=[],
+)
